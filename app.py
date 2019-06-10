5e859e11537746fd947a97ef58bc7635c2d2d588
changel <changel@changel.cn>
2019-06-10 18:31:57 +0800

    set up uwsgi

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, flash, url_for, request, redirect
 from main import forms
+import os
+import uuid
 
 app = Flask(__name__)
 
@@ -20,16 +22,27 @@ def just_flash():
     return '', 302, {'Location': '/'}
 
 
+def random_filename(filename):
+    ext = os.path.splitext(filename)[1]
+    new_filename = uuid.uuid4().hex + ext
+    return new_filename
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = forms.LoginForm()
     if form.validate_on_submit():
         username = form.username.data
+        f = form.photo.data
+        filename = random_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        flash('Upload %s succesfully ' % filename)
         flash('Welcome you %s' % username)
         return redirect(url_for('just_flash'))
     return render_template('login.html', form=form)
 
 
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.trim_blocks = True
 app.secret_key = '123456'
--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+
+
+def is_42(message=None):
+    if message is None:
+        message = 'Must be 42'
+
+    def _is_42(form, field):
+        if field.data != 42:
+            raise ValidationError(message)
+
+    return _is_42
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired('Username is required')])
+    username = StringField('Username', validators=[DataRequired()])
+    photo = FileField('Upload image', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
     password = PasswordField('Password', validators=[DataRequired('Password is required'), Length(8, 30)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log in')
